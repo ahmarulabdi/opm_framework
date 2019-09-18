fb12c3fcdd4a996b6e689d42b04d52cc05de0a07
ahmarul abdi <rndmjck@gmail.com>
2019-09-18 21:15:00 +0700

    udah sampai tahap bikin controller gan

--- config/config.php
+++ config/config.php
@@ -2,7 +2,18 @@
 
 define('DEVELOPMENT_ENVIRONMENT', TRUE);
 define('BASE_PATH',"http://localhost/framework");
-define('DDEFA')
+define('DEFAULT_CONTROLLER','pegawai');
+
+define('DB_DRIVER', 'mysql');
+define('DB_NAME', 'pegawai');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'rootreload');
+define('DB_HOST', 'localhost');
+
+
+
+
+
 
 
  ?>
